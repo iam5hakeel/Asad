f38dba7037fd8108383373a550439e1f2dd5c192
iam5hakeel <shykiahmad@gmail.com>
2025-05-09 11:31:46 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Full Screen Iframe</title>
+    <style>
+        /* Ensure the iframe takes up the entire screen */
+        html, body {
+            height: 100%;
+            margin: 0;
+        }
+        iframe {
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+    <iframe src="https://chatgpt.com/share/681dcfbc-6d68-800f-92b5-d485e551a60a" allowfullscreen></iframe>
+</body>
+</html>
